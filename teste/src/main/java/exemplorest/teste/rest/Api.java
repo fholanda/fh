bdb962bd9ac61edeb0e937f12f54807c933132af
Fernando Holanda <fholanda@gmail.com>
2015-12-15 19:16:59 -0300

    abc
    Signed-off-by: Fernando Holanda <fholanda@gmail.com>

--- teste/src/main/java/exemplorest/teste/rest/Api.java
+++ teste/src/main/java/exemplorest/teste/rest/Api.java
@@ -3,6 +3,7 @@ package exemplorest.teste.rest;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+//teste
 @ApplicationPath("api")
 public class Api extends Application {
 
